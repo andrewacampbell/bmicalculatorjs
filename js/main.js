dfaf9b1c6a24de20997194323b24e4fbcd41d5a7
Andrew <legero_cam@yahoo.com>
2016-05-04 07:26:15 -0500

    add style.css, complete bmi calculator function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,51 @@
-$(document).ready(function(){
-    
+$(document).ready(function() {
+
     $('#sub').on('click', function(){
-         var height = document.getElementById("height").value;
+    var height = document.getElementById("height").value;
     var weight = document.getElementById("weight").value;
-    
-    var heightInMeter = height/100;
-    var weightInMeter = weight/100;
-    
-    var yourBMI = weight/(heightInMeter*heightInMeter);
-    
-    
-    //outputing bmi
-    document.getElementById("output").innerHTML = "Your BMI is " + yourBMI;
+
+    if((height%1>=0) && (weight%1>=0)) {
+      var heightInMeter = height/100;
+      var yourBMI = weight/(heightInMeter*heightInMeter);
+
+      var result = "Your BMI is " + yourBMI.toFixed(2);
+
+      if(yourBMI<=18.5){
+          result += "<br/> you are under weight";
+
+      }else if(yourBMI > 18.5 && yourBMI <= 24.9){
+
+            result += "<br/> you are normal weight";
+
+      }else if(yourBMI > 24.9 && yourBMI <= 24.9){
+
+            result += "<br/> you are overweight ";
+
+      }else{
+
+            result += "<br/> you are obese";
+      }
+
+      document.getElementById("output").innerHTML = result;
+
+    }else{
+      alert("Please enter a number");
+    }
+
     });
 });
 
+function calcuateBMI() {
 
+    var height = document.getElementById("height").value;
+    var weight = document.getElementById("weight").value;
+
+    var heightInMeter = height/100;
+    var weightInMeter = weight/100;
+
+    var yourBMI = weight/(heightInMeter*heightInMeter);
+
+
+    //outputing bmi
+    document.getElementById("output").innerHTML = "Your BMI is " + yourBMI.toFixed(2);
+}
